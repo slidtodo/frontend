18fe2119f7dded703047f2614a4f1bb05464ec93
join0life <cupshop123@gmail.com>
2026-03-12 13:53:22 +0900

    feat: TaskCard 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import TaskCard from '@/shared/components/TaskCard';
 
 export default function Home() {
-  return <TaskCard id="1" text="해야할 일 하기" />;
+  return (
+    <div className="mx-auto flex h-screen w-150 flex-col justify-center">
+      <TaskCard id="1" text="해야할 일 하기" />
+      <TaskCard id="1" text="해야할 일 하기" />
+      <TaskCard id="1" text="해야할 일 하기" />
+      <TaskCard id="1" text="해야할 일 하기" />
+    </div>
+  );
 }

1c860cde07330b64d6b44ebcab7713ca7aa069e9
ramong26 <rlatndus21049@gmail.com>
2026-03-13 08:46:20 +0900

    chore: Main - element 속성 변경 div -> main으로

--- src/features/dashboard/components/Main/index.tsx
+++ src/features/dashboard/components/Main/index.tsx
@@ -3,8 +3,8 @@ interface MainProps {
 }
 export default function Main({ children }: MainProps) {
   return (
-    <div className={`w-full py-8 px-4 md:py-12 md:px-6 lg:py-20 lg:px-[96px] transition-all duration-300`}>
+    <main className={`w-full py-8 px-4 md:py-12 md:px-6 lg:py-20 lg:px-[96px] transition-all duration-300`}>
       {children}
-    </div>
+    </main>
   );
 }

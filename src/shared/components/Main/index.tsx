71e25e03ccf11789cd7037f22a08edfd24f72323
ramong26 <rlatndus21049@gmail.com>
2026-03-13 13:13:40 +0900

    fix: Sidebar - 기존 잘못되어있던 import 수정

--- src/shared/components/Main/index.tsx
+++ src/shared/components/Main/index.tsx
@@ -3,7 +3,9 @@ interface MainProps {
 }
 export default function Main({ children }: MainProps) {
   return (
-    <main className={`w-full py-8 px-4 md:py-12 md:px-6 lg:py-20 lg:px-[96px] transition-all duration-300`}>
+    <main
+      className={`w-full px-4 py-8 pt-8 pr-[16px] pl-[15px] transition-all duration-300 md:pt-12 md:pr-[26px] md:pl-6 lg:pt-20 lg:pr-[88px] lg:pr-[158px] lg:pl-[88px]`}
+    >
       {children}
     </main>
   );

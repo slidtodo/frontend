7dc7a2b175d1cc82f6a2a86891861536589c3c1d
ramong26 <rlatndus21049@gmail.com>
2026-03-16 16:16:44 +0900

    fix: Main - h screen 추가

--- src/shared/components/Main/index.tsx
+++ src/shared/components/Main/index.tsx
@@ -4,7 +4,7 @@ interface MainProps {
 export default function Main({ children }: MainProps) {
   return (
     <main
-      className={`w-full px-4 py-8 pt-8 pr-[16px] pl-[15px] transition-all duration-300 md:pt-12 md:pr-[26px] md:pl-6 lg:pt-20 lg:pr-[88px] lg:pr-[158px] lg:pl-[88px]`}
+      className={`h-screen w-full px-4 py-8 pt-8 pr-[16px] pl-[15px] transition-all duration-300 md:pt-12 md:pr-[26px] md:pl-6 lg:pt-20 lg:pr-[88px] lg:pr-[158px] lg:pl-[88px]`}
     >
       {children}
     </main>

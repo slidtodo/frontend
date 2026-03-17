5c5183445edb906e8c11a28d4f51ffb3d253fef4
ramong26 <rlatndus21049@gmail.com>
2026-03-17 09:05:46 +0900

    임시저장

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,6 +1,6 @@
 import DashBoardSummary from '@/features/dashboard/components/DashboardSummary';
 import DashboardDetail from '@/features/dashboard/components/DashBoardDetail';
-
+import PageHeader from '@/shared/components/PageHeader';
 /**
  * TODO: SEO 최적화 필요
  * @description 해당 페이지는 서버 컴포넌트입니다. 클라이언트 컴포넌트로 변경하지 말아주세요
@@ -9,8 +9,7 @@ import DashboardDetail from '@/features/dashboard/components/DashBoardDetail';
 export default function DashboardPage() {
   return (
     <div className="flex w-full flex-col">
-      {/* TODO: 페이지 헤더 컴포넌트로 변경 필요 */}
-      <span className="hidden px-2 pb-[31px] text-2xl font-semibold md:block lg:pb-[34px]">체다치즈님의 대시보드</span>
+      <PageHeader title="체다치즈님의 대시보드" className="pb-[30px] text-black lg:pb-[34px]" />
       <DashBoardSummary />
       <DashboardDetail />
     </div>

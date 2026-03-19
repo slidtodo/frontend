19fdfa4a8bf2dba743b827d05ceaf6b1cdaa390e
join0life <cupshop123@gmail.com>
2026-03-19 13:27:26 +0900

    fix: 코드 위치 변경

--- src/features/goal/note/components/NoteItem/index.tsx
+++ src/features/goal/note/components/NoteItem/index.tsx
@@ -3,7 +3,7 @@ import EllipsisButton from '../EllipsisButton';
 import Image from 'next/image';
 import { Note } from '@/shared/types/types';
 import noteIcon from '@/features/goal/note/assets/icons/icon-note.png';
-import { TodoTitle } from '@/app/(main)/goal/[id]/note/page';
+import { TodoTitle } from '@/features/goal/note/components/TodoTitle';
 
 export default async function NoteItem({ note }: { note: Note }) {
   const createDate = formatDate(new Date(note.createdAt));
@@ -11,7 +11,7 @@ export default async function NoteItem({ note }: { note: Note }) {
     <article className="flex flex-col gap-3 rounded-[20px] bg-[#FFF] p-4 md:gap-4 md:rounded-3xl md:px-[38px] md:pt-7 md:pb-8">
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-2 md:gap-4">
-          <Image src={noteIcon} sizes="32" alt="노트 아이콘" className="md:40px" />
+          <Image src={noteIcon} sizes="32" alt="노트 아이콘" className="md:h-10 md:w-10" />
           <h1 className="text-sm font-semibold text-[#1E293B] md:text-xl">{note.title}</h1>
         </div>
         <EllipsisButton

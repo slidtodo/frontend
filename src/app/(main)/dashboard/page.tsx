6242b6c807803a682b21934d458a9fe71fb6461a
ramong26 <rlatndus21049@gmail.com>
2026-03-25 14:56:50 +0900

    chore: DashboardPage -  주석 변경

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -8,7 +8,7 @@ import { todoQueries, userQueries, goalQueries } from '@/lib/queryKeys';
 
 /**
  * @description 해당 페이지는 서버 컴포넌트입니다. 클라이언트 컴포넌트로 변경하지 말아주세요
- * 'use client'로 변경 x
+ * 'use client'로 변경하지말아주세요
  */
 export default async function DashboardPage() {
   const queryClient = new QueryClient();

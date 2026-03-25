613f92548fac6bfe4e95a12341e6511e517b3e43
ramong26 <rlatndus21049@gmail.com>
2026-03-25 14:59:05 +0900

    fix: DashboardPage - 프리페치로 인해 동적 렌더링 명령어 추가

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -6,6 +6,8 @@ import DashboardDetail from '@/features/dashboard/components/DashBoardDetail';
 
 import { todoQueries, userQueries, goalQueries } from '@/lib/queryKeys';
 
+export const dynamic = 'force-dynamic';
+
 /**
  * @description 해당 페이지는 서버 컴포넌트입니다. 클라이언트 컴포넌트로 변경하지 말아주세요
  * 'use client'로 변경하지말아주세요

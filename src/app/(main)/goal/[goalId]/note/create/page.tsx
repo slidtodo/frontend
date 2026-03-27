cedb43d30b56f44def719402d23f57e83064c5e0
join0life <cupshop123@gmail.com>
2026-03-27 13:15:34 +0900

    fix: SSR 버그 수정

--- src/app/(main)/goal/[goalId]/note/create/page.tsx
+++ src/app/(main)/goal/[goalId]/note/create/page.tsx
@@ -88,6 +88,7 @@ export default function Page() {
       Placeholder.configure({ placeholder: '이 곳을 통해 노트 작성을 시작해주세요' }),
     ],
     content,
+    immediatelyRender: false,
     onUpdate: ({ editor }) => {
       setContent(editor.getHTML());
     },

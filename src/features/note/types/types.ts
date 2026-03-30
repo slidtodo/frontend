66b8f8edb31aa7be37987986ba2ae866d16e4a07
join0life <cupshop123@gmail.com>
2026-03-30 09:07:03 +0900

    style: 주석 추가

--- src/features/note/types/types.ts
+++ src/features/note/types/types.ts
@@ -2,6 +2,9 @@ import { TodoResponse } from '@/lib/api/fetchTodos';
 import { GoalDetailResponse } from '@/lib/api/fetchGoals';
 import { Editor } from '@tiptap/react';
 
+/**
+ * @TODO 노트 수정 기능 만든 후 수정
+ */
 export interface NoteEditorProps {
   editor: Editor | null;
   title: string;

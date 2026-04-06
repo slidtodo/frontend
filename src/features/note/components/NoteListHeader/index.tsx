056e9f363bb8de610fc0973a89d44da1507f1f17
join0life <cupshop123@gmail.com>
2026-04-06 15:02:43 +0900

    style: 링크 컴포넌트 수정, 중복 스타일 삭제

--- src/features/note/components/NoteListHeader/index.tsx
+++ src/features/note/components/NoteListHeader/index.tsx
@@ -37,12 +37,12 @@ export default function NoteListHeader({
   const isMobile = breakpoint === 'mobile';
 
   return (
-    <section className={clsx('md:mb-12 mb-6 flex items-center justify-between md:mb-8 md:gap-4 lg:mb-12')}>
+    <section className={clsx('mb-6 flex items-center justify-between md:mb-8 md:gap-4 lg:mb-12')}>
       {!isMobile && <PageHeader title="노트 모아보기" />}
       <div className="flex w-full flex-1 justify-between gap-0 md:justify-end md:gap-2 lg:gap-4">
         <SearchInput
           placeholder="노트를 검색해주세요"
-          className="min-w-[248px] px-5 py-3 md:px-[17px] md:py-[10px] lg:w-[320px] lg:px-5 lg:py-3 border-gray-300"
+          className="min-w-[248px] border-gray-300 px-5 py-3 md:px-[17px] md:py-[10px] lg:w-[320px] lg:px-5 lg:py-3"
           value={search}
           onChange={(e) => onSearchChange(e.target.value)}
           onIconClick={onSearch}
@@ -52,10 +52,10 @@ export default function NoteListHeader({
           <Button
             variant="primary"
             onClick={onCreateNote}
-            className="ml-2 md:ml-0 flex flex-shrink-0 gap-[6px] px-5 py-3 md:px-[17px] md:py-[10px] lg:px-[18px] lg:py-[10px]"
+            className="ml-2 flex flex-shrink-0 gap-[6px] px-5 py-3 md:ml-0 md:px-[17px] md:py-[10px] lg:px-[18px] lg:py-[10px]"
           >
             <PlusIcon size={20} />
-            <span className="font-semibold transition-all duration-200 text-sm lg:text-base">노트 등록</span>
+            <span className="text-sm font-semibold transition-all duration-200 lg:text-base">노트 등록</span>
           </Button>
           <FilterButton items={items} value={sort} onValueChange={onSortChange} />
         </div>
